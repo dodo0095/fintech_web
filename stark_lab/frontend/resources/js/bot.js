d681d1db57df1ca789982bfb650d734a97f91bce
Victor.Chou <smartvic99@gmail.com>
2020-07-11 15:27:05 +0800

    - update bot page with Axios call / create preloader

--- stark_lab/frontend/resources/js/bot.js
+++ stark_lab/frontend/resources/js/bot.js
@@ -1,6 +1,99 @@
-// const $ = require('jquery');
+const $ = require('jquery');
+const axios = require('axios');
 
-// const $body = $('html, body');
-// console.log('body', $body);
-console.log('Bot Page JS handler');
+console.log('Current Page: bot.html');
 
+const PRELOAD_DURATION = 1000;
+
+const handleAxiosGetData = () => {
+  const $s2 = $('.s2');
+  const $s3 = $('.s3');
+  const $s2MonthScore = $('.s2 .score-month');
+  const $s2SeasonScore = $('.s2 .score-season');
+  const $s2YearScore = $('.s2 .score-year');
+
+  const $s3BasicScore = $('.s3 .content-basic');
+  const $s3TechnicScore = $('.s3 .content-technic');
+
+  const $preloader = $('.preloader');
+
+  function _initSection2Data(apiData) {
+    $s2MonthScore.find('.score-value-1 .score-value-data').html(`${apiData.monthly_return}%`);
+      $s2MonthScore.find('.score-value-2 .score-value-data').html(`${apiData.monthly_0050_return}%`);
+
+      $s2SeasonScore.find('.score-value-1 .score-value-data').html(`${apiData.season_return}%`);
+      $s2SeasonScore.find('.score-value-2 .score-value-data').html(`${apiData.season_0050_return}%`);
+
+      $s2YearScore.find('.score-value-1 .score-value-data').html(`${apiData.year_return}%`);
+      $s2YearScore.find('.score-value-2 .score-value-data').html(`${apiData.year_0050_return}%`);
+
+      // Init Increase & Reduce
+      if (apiData.monthly_return[0].includes('-')) {
+        $s2MonthScore.find('.score-value-1 .score-value-data').addClass('reduce')
+      } else {
+        $s2MonthScore.find('.score-value-1 .score-value-data').addClass('increase')
+      }
+
+      if (apiData.monthly_0050_return[0].includes('-')) {
+        $s2MonthScore.find('.score-value-2 .score-value-data').addClass('reduce')
+      } else {
+        $s2MonthScore.find('.score-value-2 .score-value-data').addClass('increase')
+      }
+
+      if (apiData.season_return[0].includes('-')) {
+        $s2SeasonScore.find('.score-value-1 .score-value-data').addClass('reduce')
+      } else {
+        $s2SeasonScore.find('.score-value-1 .score-value-data').addClass('increase')
+      }
+
+      if (apiData.season_0050_return[0].includes('-')) {
+        $s2SeasonScore.find('.score-value-2 .score-value-data').addClass('reduce')
+      } else {
+        $s2SeasonScore.find('.score-value-2 .score-value-data').addClass('increase')
+      }
+
+      if (apiData.year_return[0].includes('-')) {
+        $s2YearScore.find('.score-value-1 .score-value-data').addClass('reduce')
+      } else {
+        $s2YearScore.find('.score-value-1 .score-value-data').addClass('increase')
+      }
+
+      if (apiData.year_0050_return[0].includes('-')) {
+        $s2YearScore.find('.score-value-2 .score-value-data').addClass('reduce')
+      } else {
+        $s2YearScore.find('.score-value-2 .score-value-data').addClass('increase')
+      }
+  }
+
+  function _initSection3Data(apiData) {
+    $s3BasicScore.find('.score-year .s3-content-score-value').html(`${apiData.fundamental_return}%`);
+    $s3BasicScore.find('.score-max .s3-content-score-value').html(`${apiData.fundamental_amplitude}%`);
+
+    $s3TechnicScore.find('.score-year .s3-content-score-value').html(`${apiData.technology_return}%`);
+    $s3TechnicScore.find('.score-max .s3-content-score-value').html(`${apiData.technology_amplitude}%`);
+  }
+
+  // Call Api
+  axios.get('../apiFakeData/bot.json')
+    .then((res) => {
+      const PERFORMANCE_DATA = res.data.performance;
+      const TARGET_DATA = res.data.target;
+      // Import Value
+      _initSection2Data(PERFORMANCE_DATA);
+      _initSection3Data(TARGET_DATA);
+
+      setTimeout(() => {
+        $s2.fadeIn(300);
+        $s3.fadeIn(300);
+        $preloader.addClass('js-hide');
+      }, PRELOAD_DURATION);
+
+      console.log('Axios Success');
+    })
+    .catch((error) => {
+      console.log('Axios Error');
+    })
+}
+
+handleAxiosGetData();
+  
